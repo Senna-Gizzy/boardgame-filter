a4c4dc1e4ff051c38148c0afd22779b03368d68f
Senna-Gizzy <sennabeetje@gmail.com>
2026-02-24 13:42:39 +0100

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ amount_player = st.number_input("Amount of Players", min_value=1, step=1)
 # min_player = st.text_input("Min Players")
 # max_player = st.text_input("Max Players")
 language = st.text_input("Language")
-game_type = st.selectbox('Game Type', ['Children's','Family', 'Party' ,'Strategy', 'Thematic'])
+game_type = st.selectbox('Game Type', ["Children's", 'Family', 'Party', 'Strategy', 'Thematic'])
 
 # game_type = st.text_input("Game Type")
 
