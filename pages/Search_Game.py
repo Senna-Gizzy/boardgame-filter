3a7c9c1f762bd1003a793bbb6e6eadb9abe52127
Senna-Gizzy <sennabeetje@gmail.com>
2026-02-24 20:40:47 +0100

    Create Search_Game.py

---
+++ pages/Search_Game.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import pandas as pd
+
+df = pd.read_csv("Spellenhuis Bordspellen Library - Library.csv")
+
+st.title("🔍 Search for a Specific Game")
+
+# Back button
+if st.button("⬅ Back to Home"):
+    st.switch_page("app.py")
+
+search_term = st.text_input("Type a game name")
+
+filtered = df.copy()
+
+if search_term:
+    filtered = filtered[
+        filtered['Boardgame'].str.contains(search_term, case=False, na=False)
+    ]
+
+st.dataframe(filtered)
