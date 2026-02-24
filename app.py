00f895d09d6a1061ea985d5a1d59b39edd40f908
Senna-Gizzy <sennabeetje@gmail.com>
2026-02-24 21:32:22 +0100

    Update app.py

--- app.py
+++ app.py
@@ -16,6 +16,9 @@ import pandas as pd
 df = pd.read_csv("Spellenhuis Bordspellen Library - Library.csv")
 
 df['Type'] = df['Type'].str.split(',').str[-1].str.strip()
+df['Playing Time'] = df['Playing Time'].str[:-3].str.strip()
+df[['Min Playing Time', 'Max Playing Time']] = (df['Playing Time'].str.split('-', expand=True))
+df['Max Playing Time'] = df['Max Playing Time'].fillna(df['Min Playing Time'])
 
 st.title("Boardgame Filter")
 
@@ -25,6 +28,7 @@ if st.button("Search for a Specific Game"):
 amount_player = st.number_input("Amount of Players", min_value=1, step=1)
 language = st.selectbox('Language', [None, 'Dutch', 'English'])
 game_type = st.selectbox('Game Type', [None,"Children's", 'Family', 'Party', 'Strategy', 'Thematic'])
+playing_time = st.number_input("Maximum Playing Time, min_value=10, step=5)
 
 # game_type = st.text_input("Game Type")
 
@@ -43,6 +47,9 @@ if st.button("Filter Games"):
 
     if game_type is not None:
         filtered = filtered[filtered['Type'] == game_type]
+
+    if playing_time is not None:
+        filtered = filtered[filtered['Max Playing Time'] <= playing_time]
 
     if filtered.empty:
         st.warning("No game can be found")
