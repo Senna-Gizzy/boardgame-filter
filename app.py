1430431af1c1d57de749e9256c81ad280b946e04
Senna-Gizzy <sennabeetje@gmail.com>
2026-02-24 14:04:24 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
 import streamlit as st
 import pandas as pd
 
+# TO DO
+# Search balk toevoegen
+# Talen er goed in zetten
+# Spel Mechanism Toevoegen
+# Spelduur toevoegen
+# Misschien toggle bar om aan te geven of je op iets wilt filteren
+# Complexity toevoegen
+# Plek in de kast aangeven
+# Zodra je op het spel klikt je een overzicht krijgt van hoe het eruit ziet en waar in de kast het ligt
+
+
 # Load your data (adjust filename!)
 df = pd.read_csv("Spellenhuis Bordspellen Library - Library.csv")
 
@@ -9,10 +20,6 @@ df['Type'] = df['Type'].str.split(',').str[-1].str.strip()
 st.title("🎲 Boardgame Filter")
 
 amount_player = st.number_input("Amount of Players", min_value=1, step=1)
-# amount_player = st.text_input("Amount of Players")
-# min_player = st.text_input("Min Players")
-# max_player = st.text_input("Max Players")
-# language = st.text_input("Language")
 language = st.selectbox('Language', [None, 'Dutch', 'English'])
 game_type = st.selectbox('Game Type', ["Children's", 'Family', 'Party', 'Strategy', 'Thematic'])
 
@@ -20,18 +27,10 @@ game_type = st.selectbox('Game Type', ["Children's", 'Family', 'Party', 'Strateg
 
 if st.button("Filter Games"):
     amount_player = int(amount_player) if amount_player else None
-    # min_player = int(min_player) if min_player else None
-    # max_player = int(max_player) if max_player else None
     language = language if language else None
     game_type = game_type if game_type else None
 
     filtered = df.copy()
-
-    # if min_player is not None:
-    #     filtered = filtered[filtered['Min. Players'] == min_player]
-
-    # if max_player is not None:
-    #     filtered = filtered[filtered['Max. Players'] == max_player]
 
     if amount_player is not None:
         filtered = filtered[(filtered['Min. Players'] <= amount_player) & (filtered['Max. Players'] >= amount_player)]
