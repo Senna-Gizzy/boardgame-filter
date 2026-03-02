145d1c761adc9abb9f5031a8babe3422c4c0fd69
Senna-Gizzy <sennabeetje@gmail.com>
2026-03-02 15:37:13 +0100

    Create Gurus.py

---
+++ pages/Gurus.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import pandas as pd
+
+df = pd.read_csv("Spellenhuis Bordspellen Library - Library.csv")
+
+st.title("Guru's Only")
+
+# Back button
+if st.button("⬅ Back to Home"):
+    st.switch_page("app.py")
+
+search_term = st.text_input("Type a game name")
+
+filtered = df.copy()
+
+if search_term:
+    filtered = filtered[
+        filtered['Boardgame'].str.contains(search_term, case=False, na=False)
+    ]
+
+st.dataframe(filtered)
