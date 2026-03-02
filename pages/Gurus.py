bcef2cb5222e23b3bf501dba7109d66e8eb5ac2d
Senna-Gizzy <sennabeetje@gmail.com>
2026-03-02 16:01:17 +0100

    Update Gurus.py

--- pages/Gurus.py
+++ pages/Gurus.py
@@ -2,9 +2,22 @@ import streamlit as st
 import pandas as pd
 
 st.title("Guru's Only")
-st.title("Page is in working process")
 
 # Back button
 if st.button("⬅ Back to Home"):
     st.switch_page("app.py")
 
+CORRECT_PASSWORD = st.secrets["guru_password"]
+
+
+
+password = st.text_input("Enter Password", type="password")
+
+if password == CORRECT_PASSWORD:
+    st.session_state.authenticated = True
+    st.success("Access granted!")
+    st.switch_page("app.py")
+else:
+    st.error("Incorrect password")
+
+st.stop()  
