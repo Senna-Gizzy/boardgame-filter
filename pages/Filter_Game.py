ec2285b11116b08fa29af398495e5db02a1fe41d
Senna-Gizzy <sennabeetje@gmail.com>
2026-03-02 16:38:58 +0100

    Update Filter_Game.py

--- pages/Filter_Game.py
+++ pages/Filter_Game.py
@@ -3,7 +3,10 @@
 import streamlit as st
 import pandas as pd
 
-df = pd.read_csv("Spellenhuis Bordspellen Library - Library.csv")
+sheet_url = "https://docs.google.com/spreadsheets/d/1Lf9Rs121pEpCPjAhEC7pk00ffvmOim51vLmrUePJP5A/export?format=csv&gid=0"
+df = pd.read_csv(sheet_url)
+
+# df = pd.read_csv("Spellenhuis Bordspellen Library - Library.csv")
 #_____________________________________________________________________________________________
 # Prepare dataframe
 df['Type'] = df['Type'].str.split(',').str[-1].str.strip()
