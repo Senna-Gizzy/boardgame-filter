2551325805adfa719d5ba2a2ad0c28ab8632d6f8
Senna-Gizzy <sennabeetje@gmail.com>
2026-03-02 15:45:27 +0100

    Update Gurus.py

--- pages/Gurus.py
+++ pages/Gurus.py
@@ -1,21 +1,10 @@
 import streamlit as st
 import pandas as pd
 
-df = pd.read_csv("Spellenhuis Bordspellen Library - Library.csv")
-
 st.title("Guru's Only")
+st.subtitle("Page is in working process")
 
 # Back button
 if st.button("⬅ Back to Home"):
     st.switch_page("app.py")
 
-search_term = st.text_input("Type a game name")
-
-filtered = df.copy()
-
-if search_term:
-    filtered = filtered[
-        filtered['Boardgame'].str.contains(search_term, case=False, na=False)
-    ]
-
-st.dataframe(filtered)
