1987113bf9d831e40510dcb1d0d39f3eb0faa8b0
Senna-Gizzy <sennabeetje@gmail.com>
2026-03-02 16:47:59 +0100

    Update Filter_Game.py

--- pages/Filter_Game.py
+++ pages/Filter_Game.py
@@ -33,7 +33,7 @@ if use_language:
 # Type
 use_type = st.toggle("Filter by Game Type")
 if use_type:
-    game_type = st.selectbox("Game Type",["Abstract","Children's", 'Family', 'Party', 'Strategy', 'Thematic'])
+    game_type = st.selectbox("Game Type",["Abstract","Children", 'Family', 'Party', 'Strategy', 'Narrative'])
 
 # Playing Time
 use_playing_time = st.toggle("Filter by Maximum Playing Time")
